ea68bb547135406716d175fa28454c685f413c8a
MrTorabekov <diyorbektorabekov08@gmail.com>
2025-04-04 11:13:33 +0500

    Commit

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ DB_HOST = os.getenv("POSTGRES_HOST")
 DB_PORT = os.getenv("POSTGRES_PORT")
 
 # DATABASE_URL = f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-DATABASE_URL = f"postgresql://neondb_owner:npg_qczbo3IxvXy8@ep-nameless-thunder-a165i082-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+DATABASE_URL = "postgresql://neondb_owner:npg_CX0PouQLw1vH@ep-divine-sun-a1fx39xw-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
 
 
